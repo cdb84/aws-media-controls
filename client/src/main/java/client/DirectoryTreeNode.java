cec515ed69c8279352d7d87adc6d18d5d118598e
Connor Berry <cdb979@gmail.com>
2020-11-26 18:47:30 -0500

    Tests seem well covered but a little bit scattered

--- client/src/main/java/client/DirectoryTreeNode.java
+++ client/src/main/java/client/DirectoryTreeNode.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.util.List;
+import java.util.Stack;
 import java.util.ArrayList;
 
 public class DirectoryTreeNode<T>{
@@ -11,6 +12,7 @@ public class DirectoryTreeNode<T>{
         this.value = null;
         this.children = new ArrayList<DirectoryTreeNode<T>>();
     }
+
     DirectoryTreeNode(T value){
         this.value = value;
         this.children = new ArrayList<DirectoryTreeNode<T>>();
@@ -18,6 +20,25 @@ public class DirectoryTreeNode<T>{
 
     public void addChild(DirectoryTreeNode<T> child){
         children.add(child);
+    }
+
+    public void addPath(Stack<T> pathItems){
+        if (pathItems.size() <= 0){
+            return;
+        }
+        // first we should look at the first item in the front of the stack and check
+        // to see if any of our children have it
+        T curItem = pathItems.pop();
+        for(DirectoryTreeNode<T> child: children){
+            if (child.value != null && child.value.equals(curItem)){
+                child.addPath(pathItems);
+                return;
+            }
+        }
+        // otherwise, we need to add a bunch of new nodes on!
+        DirectoryTreeNode<T> newChild = new DirectoryTreeNode<T>(curItem);
+        this.children.add(newChild);
+        newChild.addPath(pathItems);
     }
 
     public DirectoryTreeNode<T> findValue(T needle){
@@ -30,6 +51,22 @@ public class DirectoryTreeNode<T>{
             }
             return null;
         }
+    }
+
+    public void printTree(){
+        System.out.println(System.identityHashCode(this)+": "+this.value);
+        for (DirectoryTreeNode<T> child : children){
+            child.printTree();
+        }
+    }
+
+    public ArrayList<T> toArrayList(){
+        ArrayList<T> res = new ArrayList<T>();
+        res.add(value);
+        for (DirectoryTreeNode<T> child : children){
+            res.addAll(child.toArrayList());
+        }
+        return res;
     }
 
     public String toString(){
--- client/src/test/java/client/TestDirectoryNode.java
+++ client/src/test/java/client/TestDirectoryNode.java
@@ -1,18 +1,24 @@
 package client;
 
 import org.junit.jupiter.api.Test;
-
-import jdk.jfr.Timestamp;
+import java.util.Stack;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestDirectoryNode{
     @Test
+    public void testToString(){
+        DirectoryTreeNode<String> node = new DirectoryTreeNode<String>("hello world!");
+        assertEquals("hello world!", node.toString());
+    }
+
+    @Test
     public void testConstructor(){
         DirectoryTreeNode<String> node = new DirectoryTreeNode<String>();
         assertEquals(node.value, null);
         node = new DirectoryTreeNode<String>("hello world!");
-        assertEquals(node.value, "hello world!");
+        assertEquals("hello world!", node.value);
     }
 
     @Test
@@ -23,7 +29,7 @@ public class TestDirectoryNode{
         node.addChild(child);
         DirectoryTreeNode<String> actual = node.findValue(value);
         assertNotNull(actual);
-        assertSame(actual, child);
+        assertSame(child, actual);
     }
 
     @Test
@@ -47,6 +53,44 @@ public class TestDirectoryNode{
         DirectoryTreeNode<String> found = node.findValue(sndValue);
         assertNotNull(found);
         assertEquals(sndValue, found.value);
+    }
 
+    @Test
+    public void testNullFind(){
+        String value = "22";
+        DirectoryTreeNode<String> node = new DirectoryTreeNode<String>("olleh drowedrdl!");
+        node.addChild(new DirectoryTreeNode<String>("hello world!"));
+        node.addChild(new DirectoryTreeNode<String>("Also hello world!"));
+        DirectoryTreeNode<String> actual = node.findValue(value);
+        assertNotNull(node.findValue("hello world!"));
+        assertNull(actual);
+    }
+
+    @Test
+    public void testAddPath(){
+        Stack<String> pathStck = new Stack<String>();
+        String pathString = "etc/bin/urs/share/lol";
+
+        String [] pathArray = pathString.split("/");
+        for (int i = pathArray.length-1; i >= 0; i--){
+            pathStck.push(pathArray[i]);
+        }
+
+        DirectoryTreeNode<String> node = new DirectoryTreeNode<String>();
+        node.addPath(pathStck);
+
+        // this is smoke testing, because it doesn't actually ensure they were
+        // added in the right in order
+        for (String item : pathArray){
+            assertNotNull(node.findValue(item));
+        }
+
+        ArrayList<String> treeRep = node.toArrayList();
+        // off by one since the head node is nulled
+        assertEquals(pathArray.length + 1, treeRep.size());
+        for (int i = 0; i < pathArray.length; i++){
+            // skip our head node
+            assertEquals(pathArray[i], treeRep.get(i + 1));
+        }
     }
 }
