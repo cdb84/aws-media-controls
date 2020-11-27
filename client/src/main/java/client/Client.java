5729172d940e59eaa4fc17c78f3d3701e566e269
Connor Berry <cdb979@gmail.com>
2020-11-26 19:43:11 -0500

    Tighten up the ignore

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -4,12 +4,11 @@ import com.amazonaws.regions.Regions;
 
 public class Client{
 
-    public static void Main(String [] args){
+    public static void Main(String [] args) throws Exception{
         String bucketName = null;
         String exec;
         if (args.length <= 1){
-            throw new Exception();
-            System.exit(1);
+            throw new IllegalArgumentException();
         } 
         if (args.length == 2){
             bucketName = args[1];
