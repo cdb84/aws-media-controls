6cd3feda3b48feb28fcc8780edd4b5ae3cb24837
Connor Berry <connor@connorsco.de>
2020-11-30 17:00:47 -0500

    Little hiccup with main there---would have loved to catch that one in the unit tests

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -1,8 +1,11 @@
 package client;
 
-import com.amazonaws.regions.Regions;
+import java.io.IOException;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
 
 public class Client{
 
@@ -12,15 +15,32 @@ public class Client{
         if (args.length <= 0){
             throw new IllegalArgumentException();
         } 
-        if (args.length == 1){
+        if (args.length >= 1){
             bucketName = args[0];
         }
         if (args.length == 2){
             exec = args[1];
+        }else{
+            exec = "vlc"; // probably best default in mind
         }
         System.err.println("Initializing handler for "+bucketName+" executing "+exec);
-        Handler h = new Handler(bucketName, Regions.US_EAST_2);
+        Handler h = new Handler(bucketName, AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build());
         
         DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
+
+        Terminal terminal = null;
+        try{
+            terminal = defaultTerminalFactory.createTerminal();
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            if (terminal != null){
+                try{
+                    terminal.close();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
